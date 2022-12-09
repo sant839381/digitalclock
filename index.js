019c4e67bb661c177f86aa5e7492dc6552b3c6a2
sant839381 <santoshgangwarmca>
2022-12-10 00:02:53 +0530

    digital clock

---
+++ index.js
@@ -0,0 +1,32 @@
+function displayTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+   if(h>12){
+    h=h-12;
+   }
+    
+    if(h >=12){
+        session = "PM";
+    }
+    else{
+        session="AM";
+    }
+    
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(displayTime, 1000);
+    
+}
+
+displayTime();
